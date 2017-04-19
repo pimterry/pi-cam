2b2f711249fcd5167bfba5764d3cc4d5ec6b76ba
Tim Perry <pimterry@gmail.com>
2017-04-19 21:33:57 +0200

    Stream pi video over websockets

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-var express = require('express');
-var raspivid = require('raspivid');
-var fs = require('fs');
+const express = require('express');
+const Splitter = require('stream-split');
+const raspivid = require('raspivid');
+const fs = require('fs');
 
-const VIDEO = '/data/video.h264';
+function startRecording() {
+    const NALseparator = new Buffer([0,0,0,1]);
 
-const file = fs.createWriteStream(VIDEO);
-const video = raspivid({
-    width: 640,
-    height: 480,
-    framerate: 12,
-    profile: 'baseline'
-});
+    const video = raspivid({
+        width: 640,
+        height: 480,
+        framerate: 12,
+        profile: 'baseline'
+    });
 
-video.pipe(file);
+    video.pipe(new Splitter(NALseparator)).on('data', broadcastStream);
+}
 
-var app = express();
+const app = express();
+const wss = require('express-ws')(app);
+
+function broadcastStream(data) {
+    wss.getWss().clients.forEach((socket) => {
+        if (socket.busy) return;
+
+        socket.busy = true;
+        socket.send(Buffer.concat([NALseparator, data]), { binary: true }, (error) => {
+            console.error(error);
+            socket.busy = false;
+        });
+    });
+}
 
 app.get('/', (req, res) => {
-    res.send(200, 'Hello world');
+    res.status(200).send('Hello world');
 });
 
-app.get('/video', (req, res) => res.sendFile(VIDEO));
+app.ws('/video-stream', (ws, req) => {
+    console.log('Client connected');
 
+    ws.send(JSON.stringify({
+      action: 'init',
+      width: '640',
+      height: '480'
+    }));
+
+    ws.on('message', (msg) => console.log('Received', msg));
+
+    ws.on('close', () => console.log('Client left'));
+});
+
+startRecording();
 app.listen(80, () => console.log('Server started on 80'));
