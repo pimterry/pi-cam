4d9706ba2c6793613c0238b931163dffec621753
Tim Perry <pimterry@gmail.com>
2017-04-19 20:34:52 +0200

    Record a video on boot

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var raspivid = require('raspivid');
+var fs = require('fs');
+
+const VIDEO = '/data/video.h264';
+
+var file = fs.createWriteStream(VIDEO);
+var video = raspivid();
+
+video.pipe(file);
 
 var app = express();
 
 app.get('/', (req, res) => {
     res.send(200, 'Hello world');
 });
+
+app.get('/video', (req, res) => res.sendFile(VIDEO));
 
 app.listen(80, () => console.log('Server started on 80'));
