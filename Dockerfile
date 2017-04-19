eaa58924a6d10ed47c673d08aaa54b18a860ca83
Tim Perry <pimterry@gmail.com>
2017-04-19 19:39:36 +0200

    Basic deployable express setup

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM resin/raspberrypi3-node
 
-RUN apt-get update && apt-get install -yq libraspberrypi-bin
-
 WORKDIR usr/src/app
+
+COPY package.json ./
+RUN JOBS=MAX npm i --production
+
 COPY . ./
 
 CMD npm start
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send(200, 'Hello world');
+});
+
+app.listen(8080, () => console.log('Server started on 8080'));
