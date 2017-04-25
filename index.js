b003b24321cf98d1c6fb5bf4d2c34c29fd4e2525
Tim Perry <pimterry@gmail.com>
2017-04-25 23:44:51 +0200

    Demo start state

--- index.js
+++ index.js
@@ -9,19 +9,10 @@ const raspividStream = require('raspivid-stream');
 const app = express();
 const wss = require('express-ws')(app);
 
-const Blinkt = require('node-blinkt');
-const leds = new Blinkt();
-leds.setup();
-
-leds.setAllPixels(255, 255, 255, 1);
-leds.sendUpdate();
-setTimeout(updateLeds, 1000);
-
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.ws('/video-stream', (ws, req) => {
     console.log('Client connected');
-    updateLeds();
 
     ws.send(JSON.stringify({
       action: 'init',
@@ -37,26 +28,9 @@ app.ws('/video-stream', (ws, req) => {
 
     ws.on('close', () => {
         console.log('Client left');
-        updateLeds();
         videoStream.removeAllListeners('data');
     });
 });
-
-function updateLeds() {
-    const clientCount = wss.getWss().clients.size;
-    console.log('Updating leds to show', clientCount, 'clients');
-
-    leds.clearAll();
-
-    for (let i = 0; i < clientCount; i++) {
-        let colors = [0, 0, 0];
-        colors[Math.floor(i / 8)] = 255;
-
-        leds.setPixel(i % 8, ...colors, 1);
-    }
-
-    leds.sendUpdate();
-}
 
 app.use(function (err, req, res, next) {
   console.error(err);
